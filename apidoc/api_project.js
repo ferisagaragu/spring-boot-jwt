4f8a3d956f7196a58ae9cf56b84bad3a8f2e4c89
Fernando García <ferisagaragu@gmail.com>
2020-01-03 13:52:48 -0600

    apidoc

---
+++ apidoc/api_project.js
@@ -0,0 +1,16 @@
+define({
+  "name": "Tlozbotw - Api",
+  "version": "0.0.1",
+  "description": "Servicios web para TLOZBOTW",
+  "title": "Tlozbotw - Api",
+  "url": "http://localhost:4000/",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2020-01-03T19:31:33.434Z",
+    "url": "http://apidocjs.com",
+    "version": "0.19.1"
+  }
+});
