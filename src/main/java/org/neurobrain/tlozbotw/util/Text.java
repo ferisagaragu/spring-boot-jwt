f3b5c6e12c2c1cbcac426d656b9b031e168a0195
Fernando García <ferisagaragu@gmail.com>
2020-01-06 12:07:30 -0600

    user doc

--- src/main/java/org/neurobrain/tlozbotw/util/Text.java
+++ src/main/java/org/neurobrain/tlozbotw/util/Text.java
@@ -1,43 +1,62 @@
 package org.neurobrain.tlozbotw.util;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.lang.reflect.Method;
+
 import java.util.Base64;
 import java.util.UUID;
 
 
 public class Text {
 
+	private static final Logger logger = LoggerFactory.getLogger(Text.class);
+
+	private Text() {
+		throw new IllegalStateException("Problems in: " + Text.class);
+	}
+
 	public static String toJSONString(Object obj) {
 		try {
-			String jsonOut = "{\n";
+			StringBuilder bld = new StringBuilder();
 			Method[] methods = obj.getClass().getMethods();
-			
+			bld.append("{\n");
+
 			for (int i = 0; i < methods.length; i++) {
 				if (
-					methods[i].getName().contains("get") && 
-					!methods[i].getName().equals("getClass")
+					methods[i].getName().contains("get") &&
+					(!methods[i].getName().equals("getClass"))
 				) {
-					jsonOut += "  \"" +  
-						methods[i].getName().replace("get", "").toLowerCase() + 
-						"\":\"" + methods[i].invoke(obj) + "\",\n";
+					bld.append(
+						"  \"" + formatKey(methods[i].getName()) + "\": " +
+						"\"" + methods[i].invoke(obj) + "\",\n"
+					);
 				}
 			}
 	
-			jsonOut = jsonOut.substring(0, jsonOut.length() - 2) + "\n}";
-			
-			return jsonOut;
+			return bld.substring(0, bld.length() - 2) + "\n}";
 		} catch (Exception e) {
-			return "Problem when converting the class to json string, "
+			logger.error(
+				"Problem when converting the class to json string, "
 				 + "it is necessary to generate the getter and setter "
-				 + "methods in your entity";
-		} 
+				 + "methods in your entity"
+			);
+		}
+
+		return "";
 	}
-	
-	
+
 	public static String uniqueString() {
 		String originalInput = UUID.randomUUID().toString().substring(0, 12);
-		String encodedString = Base64.getEncoder().encodeToString(originalInput.getBytes());
-		return encodedString;
+		return Base64.getEncoder().encodeToString(originalInput.getBytes());
 	}
-	
+
+	private static Object formatKey(Object stg) {
+		String out = stg.toString()
+			.replace("get","");
+		String  initialLyrics = String.valueOf(out.charAt(0))
+			.toLowerCase();
+		return initialLyrics + out.substring(1);
+	}
 }
